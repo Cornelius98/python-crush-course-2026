5e20e07895fc673796cd9c842d51bbdf349dd404
Cornelius Kasokola <41131476+Cornelius98@users.noreply.github.com>
2026-05-25 22:51:14 +0200

    DAY 3: Data Types 1

---
+++ data_type.py
@@ -0,0 +1,33 @@
+#Data types file
+
+#Integer data type
+num_1 = 10
+print(num_1)
+
+#Float and double data types
+float_num = 3.142
+print(float_num)
+
+#Character
+char_1 = 'A'
+print(char_1)
+
+#String 
+name = 'Guido Van Rossum'
+print(name)
+
+#List (or Array)
+fruits = ["apple", "guava", "mango"]
+print(fruits)
+
+height = [1.5, 1.2, 1, 0.98, 2.5]
+print(height)
+
+#dictionary (Hash maps) key-value
+founder = {
+    "name": "Guido Van Rossum",
+    "age": 10,
+    "country": "Netherlands"
+}
+
+print(founder)
